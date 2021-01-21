99447a4861ae5a117edef82f6e9e578d7c90dda1
zhengMK <61963726+zhengMK@users.noreply.github.com>
2021-01-21 10:29:57 +0800

    2021/1/21

---
+++ Top_K_Frequent_Elements/priorityqueue_method.java
@@ -0,0 +1,37 @@
+// Use a maximum priority queue to tack the most frequent element in the array.
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            if (map.containsKey(num)) map.put(num, map.get(num) + 1);
+            else  map.put(num, 1);
+        }
+        Comparator<Node> comparator = new NodeComparator();
+        PriorityQueue<Node> pq = new PriorityQueue<>(nums.length, comparator);
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            pq.add(new Node(entry.getKey(), entry.getValue()));
+        }
+        int[] ret = new int[nums.length];
+        int index = 0;
+        for (int i = 0; i < k; i++) {
+            ret[index++] = pq.remove().val;
+        }
+        return Arrays.copyOfRange(ret, 0, index);
+    }
+    
+    private class NodeComparator implements Comparator<Node> {
+        @Override
+        public int compare(Node x, Node y){
+            return y.count - x.count;
+        }
+    }
+    
+    class Node {
+        int val;
+        int count;
+        public Node (int val, int count) {
+            this.val = val;
+            this.count = count;
+        }
+    }
+} 
