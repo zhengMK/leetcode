afcdb3fcbe3231ee61a66e906a74028e30263b7f
zhengMK <61963726+zhengMK@users.noreply.github.com>
2021-03-22 19:11:33 +0800

    Update XOR.java

--- Find the Difference/XOR.java
+++ Find the Difference/XOR.java
@@ -1,15 +1,13 @@
 // Only the added new char will appear once. 
 // All the common chars in s and t will appear twice.
 // So, can us XOR operation to find the single item. (Single number problem)
-// Time complexity: O(s.lenght + t.length), space complexity: O(s.lenght + t.length)
+// Time complexity: O(s.lenght + t.length), space complexity: O(1)
 class Solution {
     public char findTheDifference(String s, String t) {
-        StringBuilder sb = new StringBuilder(s);
-        sb.append(t);
         char result = 0;
-        for(int i = 0; i < sb.length(); i++) {
-            result ^= sb.charAt(i);
+        for(int i = 0; i < s.length(); i++) {
+            result ^= s.charAt(i) ^ t.charAt(i);
         }
-        return result;
+        return (char) (result ^ t.charAt(t.length() - 1));
     }
 }
